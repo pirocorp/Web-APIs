c4024e7912137ed47377007c1d32a4850b396f66
Zdravko Zdravkov <zdr0@yahoo.com>
2023-08-13 14:00:51 +0200

    Add Response Service

---
+++ Polly/ResponseService/Controllers/ResponseController.cs
@@ -0,0 +1,30 @@
+﻿namespace ResponseService.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ResponseController : ControllerBase
+    {
+        /// <summary>
+        /// Returns randomly successful response. The higher the Id the probability of the successful response is also higher 
+        /// </summary>
+        /// <param name="id">The id of the response</param>
+        /// <returns>Response</returns>
+        [HttpGet]
+        [Route("id:int")]
+        public async Task<ActionResult> GetResponse(int id)
+        {
+            Random random = new Random();
+            var randomInteger = random.Next(1, 101);
+
+            if (randomInteger >= id)
+            {
+                Console.WriteLine("--> Failure - Generate a HTTP 500 <--");
+                return await Task.FromResult(this.StatusCode(StatusCodes.Status500InternalServerError));
+            }
+
+            return await Task.FromResult(this.Ok());
+        }
+    }
+}
